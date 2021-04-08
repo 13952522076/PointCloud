ed42a8553a08788bd956a6c7196e5a86fb5dd979
13952522076 <xuma@my.unt.edu>
2021-04-08 02:13:47 -0400

    update

--- classification.py
+++ classification.py
@@ -99,8 +99,8 @@ def main():
         logger.append([epoch, optimizer.param_groups[0]['lr'],
                               train_out["loss"], train_out["acc"],
                               test_out["loss"], test_out["acc"]])
-        print(f"Training(loss: {train_out['loss']} acc:{train_out['acc']} time:{train_out['time']}) | "
-              f"Testing(loss: {test_out['loss']} acc:{test_out['acc']} time:{test_out['time']})")
+        print(f"Training(loss: {train_out['loss']} acc:{train_out['acc']}% time:{train_out['time']}s) | "
+              f"Testing(loss: {test_out['loss']} acc:{test_out['acc']}% time:{test_out['time']}s)")
     logger.close()
 
 
