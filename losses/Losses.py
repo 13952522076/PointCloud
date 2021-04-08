42a6fe22752b086b92687807362bc2193103bd40
13952522076 <xuma@my.unt.edu>
2021-04-07 21:51:29 -0400

    update

--- losses/Losses.py
+++ losses/Losses.py
@@ -2,19 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+__all__ = ['PointNetLoss', '']
 
-__all__ = ['PointNetLoss','']
+
 class PointNetLoss(nn.Module):
     def __init__(self, mat_diff_loss_scale=0.001):
         super(PointNetLoss, self).__init__()
         self.mat_diff_loss_scale = mat_diff_loss_scale
         self.ce = nn.CrossEntropyLoss()
-        self.I = torch.eye(64)[None, :, :]
+        self.register_buffer("I", torch.eye(64)[None, :, :])
 
     def feature_transform_reguliarzer(self, trans):
         loss = torch.mean(torch.norm(torch.bmm(trans, trans.transpose(2, 1)) - self.I, dim=(1, 2)))
         return loss
-
 
     def forward(self, net_out, targets):
         logits = net_out["logits"]
@@ -33,4 +33,3 @@ class CELoss(nn.Module):
     def forward(self, net_out, targets):
         logits = net_out["logits"]
         return self.ce(logits, targets)
-
