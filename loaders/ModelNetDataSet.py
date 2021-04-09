f7ed1410e3d6e26e85685e5b1896ec5eb0634bfb
13952522076 <xuma@my.unt.edu>
2021-04-08 20:44:07 -0400

    update

--- loaders/ModelNetDataSet.py
+++ loaders/ModelNetDataSet.py
@@ -47,97 +47,6 @@ def farthest_point_sample(point, npoint):
     return point
 
 
-class ModelNetDataLoader(Dataset):
-    def __init__(self, root, args, split='train', process_data=False):
-        self.root = root
-        self.npoints = args.num_points
-        self.process_data = process_data
-        self.uniform = args.use_uniform_sample
-        self.use_normals = args.use_normals
-        self.num_classes = args.num_classes
-
-        if self.num_classes == 10:
-            self.catfile = os.path.join(self.root, 'modelnet10_shape_names.txt')
-        else:
-            self.catfile = os.path.join(self.root, 'modelnet40_shape_names.txt')
-
-        self.cat = [line.rstrip() for line in open(self.catfile)]
-        self.classes = dict(zip(self.cat, range(len(self.cat))))
-
-        shape_ids = {}
-        if self.num_classes == 10:
-            shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet10_train.txt'))]
-            shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet10_test.txt'))]
-        else:
-            shape_ids['train'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_train.txt'))]
-            shape_ids['test'] = [line.rstrip() for line in open(os.path.join(self.root, 'modelnet40_test.txt'))]
-
-        assert (split == 'train' or split == 'test')
-        shape_names = ['_'.join(x.split('_')[0:-1]) for x in shape_ids[split]]
-        self.datapath = [(shape_names[i], os.path.join(self.root, shape_names[i], shape_ids[split][i]) + '.txt') for i
-                         in range(len(shape_ids[split]))]
-        print('The size of %s data is %d' % (split, len(self.datapath)))
-
-        if self.uniform:
-            self.save_path = os.path.join(root,
-                                          'modelnet%d_%s_%dpts_fps.dat' % (self.num_classes, split, self.npoints))
-        else:
-            self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts.dat' % (self.num_classes, split, self.npoints))
-
-        if self.process_data:
-            if not os.path.exists(self.save_path):
-                print('Processing data %s (only running in the first time)...' % self.save_path)
-                self.list_of_points = [None] * len(self.datapath)
-                self.list_of_labels = [None] * len(self.datapath)
-
-                for index in tqdm(range(len(self.datapath)), total=len(self.datapath)):
-                    fn = self.datapath[index]
-                    cls = self.classes[self.datapath[index][0]]
-                    cls = np.array([cls]).astype(np.int32)
-                    point_set = np.loadtxt(fn[1], delimiter=',').astype(np.float32)
-
-                    if self.uniform:
-                        point_set = farthest_point_sample(point_set, self.npoints)
-                    else:
-                        point_set = point_set[0:self.npoints, :]
-
-                    self.list_of_points[index] = point_set
-                    self.list_of_labels[index] = cls
-
-                with open(self.save_path, 'wb') as f:
-                    pickle.dump([self.list_of_points, self.list_of_labels], f)
-            else:
-                print('Load processed data from %s...' % self.save_path)
-                with open(self.save_path, 'rb') as f:
-                    self.list_of_points, self.list_of_labels = pickle.load(f)
-
-    def __len__(self):
-        return len(self.datapath)
-
-    def _get_item(self, index):
-        if self.process_data:
-            point_set, label = self.list_of_points[index], self.list_of_labels[index]
-        else:
-            fn = self.datapath[index]
-            cls = self.classes[self.datapath[index][0]]
-            label = np.array([cls]).astype(np.int32)
-            point_set = np.loadtxt(fn[1], delimiter=',').astype(np.float32)
-
-            if self.uniform:
-                point_set = farthest_point_sample(point_set, self.npoints)
-            else:
-                point_set = point_set[0:self.npoints, :]
-
-        point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
-        if not self.use_normals:
-            point_set = point_set[:, 0:3]
-
-        return point_set, label[0]
-
-    def __getitem__(self, index):
-        return self._get_item(index)
-
-
 class ModelNet40DataSet(Dataset):
     class_num = 40
 
@@ -165,22 +74,29 @@ class ModelNet40DataSet(Dataset):
         self.processed_file = "modelnet%s_%s_%spts_%s.npz" % (self.class_num, temp_Train, self.points, temp_uniform)
         if process_data:
             print(f"===> processing data: {self.processed_file}")
-            self._process_data()
+            if os.path.isfile(os.path.join(self.root, self.processed_file)):
+                print(f"===> File {self.processed_file} exsits, ignoring processing.")
+            else:
+                self._process_data()
 
         # now load the picked numpy arrays
         processed_data = np.load(os.path.join(self.root, self.processed_file))
         self.data = processed_data["data"]
         self.targets = processed_data["targets"]
 
-    # def __getitem__(self, index): return None
-    # def __len__(self): return None
+    def __getitem__(self, index):
+        data, target = self.data[index], self.targets[index]
+        data[:, 0:3] = pc_normalize(data[:, 0:3])
+        return data, target
+
+    def __len__(self):
+        return len(self.data)
 
     def _process_data(self):
         points = []
         targets = []
         file_names = [line.rstrip() for line in open(os.path.join(self.root, self.datafile))]
-        # for i in tqdm(range(len(file_names))):
-        for i in tqdm(range(10)):
+        for i in tqdm(range(len(file_names))):
             file = file_names[i]
             class_temp = file.split("_")[0]
             file_data = np.genfromtxt(os.path.join(self.root, class_temp, file + '.txt'),
@@ -193,9 +109,13 @@ class ModelNet40DataSet(Dataset):
             points.append(file_data)
             targets.append(file_target)
         # return points, targets
-        np.savez_compressed(os.path.join(self.root, self.processed_file),data=points,targets=targets)
+        np.savez_compressed(os.path.join(self.root, self.processed_file), data=points, targets=targets)
 
 
 if __name__ == '__main__':
-    modelnet40 = ModelNet40DataSet(root="/work/xm0036/data/modelnet40_normal_resampled",
+    modelnet40_train_dataset = ModelNet40DataSet(root="/work/xm0036/data/modelnet40_normal_resampled",
                                    train=True, points=1024, use_uniform_sample=True, process_data=True)
+    modelnet40_test_dataset = ModelNet40DataSet(root="/work/xm0036/data/modelnet40_normal_resampled",
+                                   train=False, points=1024, use_uniform_sample=True, process_data=True)
+    print(modelnet40_train_dataset.__len__())
+    print(modelnet40_test_dataset.__len__())
