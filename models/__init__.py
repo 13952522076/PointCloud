1d23aabf72c6c97dd2c9e5312d2c0a918a59cb6e
13952522076 <xuma@my.unt.edu>
2021-04-14 18:51:34 -0400

    update

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from .pointnet_cls import PointNet
 from .pointnet2_cls_ssg import PointNet2SSG
 from .point_transformer import PointTransformer
 from .pointsformer import *
+from .Simpler import Simpler
