a38afb27cb8eeeab2fd1451319144d76cc321d54
13952522076 <xuma@my.unt.edu>
2021-04-14 21:13:56 -0400

    update

---
+++ count_Param.py
@@ -0,0 +1,14 @@
+from flops_counter import get_model_complexity_info
+import models as models
+
+model = models.__dict__['Simpler'](num_classes=40)
+
+flops, params = get_model_complexity_info(model, 1024, as_strings=False, print_per_layer_stat=False, channel=6)
+print('Flops:  %.3f' % (flops / 1e9))
+print('Params: %.2fM' % (params / 1e6))
+# print(model)
+# print(flops)
+# print(params)
+
+# model = models.__dict__['se_mnasnet1_0'](num_classes=1000)
+# print(model)
--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ from .pointnet2_cls_ssg import PointNet2SSG
 from .point_transformer import PointTransformer
 from .pointsformer import *
 from .Simpler import Simpler
+from .Simpler2 import Simpler2A, Simpler2B, Simpler2C, Simpler2D, Simpler2E
